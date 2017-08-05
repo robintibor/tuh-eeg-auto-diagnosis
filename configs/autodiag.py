ba186feff53b030cea72f538e02bc2861602cf70
Robin Tibor Schirrmeister <robintibor@gmail.com>
2017-08-05 21:19:25 +0200

    preproc clean etc

--- configs/autodiag.py
+++ configs/autodiag.py
@@ -16,9 +16,9 @@ from hyperoptim.parse import cartesian_dict_of_lists_product, \
 from hyperoptim.util import save_pkl_artifact
 from autoeeglukas.utils.my_io import (time_key, read_all_file_names,
                                       get_info_with_mne)
-from braindecode.datautil.signalproc import exponential_running_standardize
+from braindecode.datautil.signalproc import (exponential_running_standardize,
+    exponential_running_demean)
 from braindecode.datautil.signal_target import SignalAndTarget
-from braindecode.datautil.splitters import split_into_two_sets
 from braindecode.torch_ext.util import np_to_var
 from braindecode.torch_ext.util import set_random_seeds
 from braindecode.experiments.experiment import Experiment
@@ -30,7 +30,8 @@ from braindecode.experiments.stopcriteria import MaxEpochs
 from braindecode.models.shallow_fbcsp import ShallowFBCSPNet
 from braindecode.models.deep4 import Deep4Net
 from braindecode.models.util import to_dense_prediction_model
-
+from braindecode.datautil.iterators import get_balanced_batches
+from braindecode.datautil.signalproc import bandpass_cnt
 
 log = logging.getLogger(__name__)
 log.setLevel('DEBUG')
@@ -48,19 +49,67 @@ def get_grid_param_list():
 
     load_params = [{
         'max_recording_mins': 35,
-        'n_recordings': 150
+        'n_recordings': 10#150
     }]
+
+    clean_params = [
+        {'max_min_threshold': None,
+         'max_min_expected': None,
+         'shrink_the_spikes': False},
+        {'max_min_threshold': None,
+         'max_min_expected': None,
+         'shrink_the_spikes': True},
+        {'max_min_threshold': 600,
+         'max_min_expected': 50,
+         'shrink_the_spikes': False},
+    ]
 
     preproc_params = dictlistprod({
         'sec_to_cut': [60],
         'duration_recording_mins': [3],
-        'max_abs_val': [None, 800, ],
+        'max_abs_val': [None],# 800, ],
         'sampling_freq': [100],
+        'low_cut_hz': [None],# 0.1],
+        'high_cut_hz': [None],# 45],
     })
 
+    standardizing_defaults = {
+        'exp_demean': False,
+        'exp_standardize': False,
+        'moving_demean': False,
+        'moving_standardize': False,
+        'channel_demean': False,
+        'channel_standardize': False,
+    }
+
+    standardizing_variants = [
+        {},
+        {
+            'exp_demean': True,
+        },
+        {
+            'exp_standardize': True,
+        },
+        {
+            'moving_demean': True,
+        },
+        {
+            'moving_standardize': True,
+        },
+        {
+            'channel_demean': True,
+        },
+        {
+            'channel_standardize': True,
+        },
+    ]
+
+    standardizing_params = product_of_list_of_lists_of_dicts(
+        [[standardizing_defaults], ])#standardizing_variants
+
     split_params = [{
-        'test_fraction': 0.25,
-        'valid_of_train_fraction': 0.2
+        'n_folds': 5,
+        'i_test_fold': 4,
     }]
 
     model_params = [{
@@ -80,16 +129,20 @@ def get_grid_param_list():
     }]
 
     stop_params = [{
-        'max_epochs': 35,
+        'max_epochs': 3,#35,
     }]
+
+
 
     grid_params = product_of_list_of_lists_of_dicts([
         default_params,
         load_params,
+        clean_params,
         preproc_params,
         split_params,
         model_params,
         iterator_params,
+        standardizing_params,
         stop_params
     ])
 
@@ -182,24 +235,148 @@ class DiagnosisSet(object):
         return X, y
 
 
+def create_set(X, y, inds):
+    """
+    X list and y nparray
+    :return: 
+    """
+    new_X = []
+    for i in inds:
+        new_X.append(X[i])
+    new_y = y[inds]
+    return SignalAndTarget(new_X, new_y)
+
+
 class Splitter(object):
-    def __init__(self, test_fraction, valid_of_train_fraction):
-        self.test_fraction = test_fraction
-        self.valid_of_train_fraction = valid_of_train_fraction
+    def __init__(self, n_folds, i_test_fold):
+        self.n_folds = n_folds
+        self.i_test_fold = i_test_fold
 
     def split(self, X, y):
-        n_split = int((1.0 - self.test_fraction) * len(y))
-        train_set = SignalAndTarget(X[:n_split], y=y[:n_split])
-        test_set = SignalAndTarget(X[n_split:], y=y[n_split:])
-        first_set_fraction = 1 - self.valid_of_train_fraction
-        train_set, valid_set = split_into_two_sets(
-            train_set, first_set_fraction=first_set_fraction)
+        folds = get_balanced_batches(len(X), None, False,
+                                     n_batches=self.n_folds)
+        test_inds = folds[self.i_test_fold]
+        valid_inds = folds[self.i_test_fold - 1]
+        all_inds = list(range(len(X)))
+        train_inds = np.setdiff1d(all_inds, np.union1d(test_inds, valid_inds))
+        assert np.intersect1d(train_inds, valid_inds).size == 0
+        assert np.intersect1d(train_inds, test_inds).size == 0
+        assert np.intersect1d(valid_inds, test_inds).size == 0
+        assert np.array_equal(np.sort(
+            np.union1d(train_inds, np.union1d(valid_inds, test_inds))),
+            all_inds)
+
+        train_set = create_set(X, y, train_inds)
+        valid_set = create_set(X, y, valid_inds)
+        test_set = create_set(X, y, test_inds)
+
         return train_set, valid_set, test_set
 
 
+def running_mean(arr, window_len, axis=0):
+    # adapted from http://stackoverflow.com/a/27681394/1469195
+    # need to pad to get correct first value also
+    arr_padded = np.insert(arr,0,values=0,axis=axis)
+    cumsum = np.cumsum(arr_padded,axis=axis)
+    later_sums = np.take(cumsum, range(window_len, arr_padded.shape[axis]),
+        axis=axis)
+    earlier_sums = np.take(cumsum, range(0, arr_padded.shape[axis] - window_len),
+        axis=axis)
+
+    moving_average = (later_sums - earlier_sums) / float(window_len)
+    return moving_average
+
+
+def padded_moving_mean(arr, axis, n_window):
+    """Pads by replicating n_window first and last elements
+    and putting them at end and start (no reflection)"""
+    start_pad_inds = list(range(0, n_window // 2))
+    end_pad_inds = list(range(arr.shape[axis] - (n_window // 2),
+                              arr.shape[axis]))
+    arr = np.concatenate((arr.take(start_pad_inds, axis=axis),
+                          arr,
+                          arr.take(end_pad_inds, axis=axis)),
+                         axis=axis)
+    mov_mean = running_mean(arr, window_len=n_window, axis=axis)
+    return mov_mean
+
+
+def padded_moving_demean(arr, axis, n_window):
+    assert arr.dtype != np.float16
+    assert n_window % 2 == 1
+    mov_mean = padded_moving_mean(arr, axis, n_window=n_window)
+    arr = arr - mov_mean
+    return arr
+
+
+def padded_moving_divide_square(arr, axis, n_window, eps=0.1):
+    """Pads by replicating n_window first and last elements
+    and putting them at end and start (no reflection)"""
+    assert arr.dtype != np.float16
+    assert n_window % 2 == 1
+    mov_mean_sqr = padded_moving_mean(np.square(arr), axis=axis, n_window=n_window)
+    arr = arr / np.maximum(np.sqrt(mov_mean_sqr), eps)
+    return arr
+
+
+def padded_moving_standardize(arr, axis, n_window, eps=0.1):
+    assert arr.dtype != np.float16
+    assert n_window % 2 == 1
+    arr = padded_moving_demean(arr, axis=axis, n_window=n_window)
+    arr = padded_moving_divide_square(arr, axis=axis, n_window=n_window, eps=eps)
+    return arr
+
+
+def demean(x, axis):
+    return x - np.mean(x, axis=axis, keepdims=True)
+
+def standardize(x, axis):
+    return (x - np.mean(x, axis=axis, keepdims=True)) / np.maximum(
+        1e-4, np.std(x, axis=axis, keepdims=True))
+
+
+def clean_jumps(x, window_len, threshold, expected, cuda):
+    x_var = np_to_var([x])
+    if cuda:
+        x_var = x_var.cuda()
+
+    maxs = F.max_pool1d(x_var,window_len, stride=1)
+    mins = F.max_pool1d(-x_var,window_len, stride=1)
+
+    diffs = maxs + mins
+    large_diffs = (diffs > threshold).type_as(diffs) * diffs
+    padded = F.pad(large_diffs.unsqueeze(0), (window_len-1,window_len-1, 0,0), 'constant', 0)
+    max_diffs = th.max(padded[:,:,:,window_len-1:], padded[:,:,:,:-window_len+1]).unsqueeze(0)
+    max_diffs = th.clamp(max_diffs, min=expected)
+    x_var = x_var * (expected / max_diffs)
+
+    x = x_var.data.cpu().numpy()[0]
+    return x
+
+
+def shrink_spikes(example, threshold, axis, n_window):
+    """Example could be single example or all...
+    should work for both."""
+    run_mean = padded_moving_mean(example.astype(np.float32),
+        axis=axis, n_window=n_window)
+    abs_run_mean = np.abs(run_mean)
+    is_relevant = (abs_run_mean > threshold)
+
+    cleaned_example = example - is_relevant * (run_mean - (
+             np.sign(run_mean) * (threshold +
+            np.log(np.maximum(abs_run_mean - threshold + 1, 0.01)))))
+    return cleaned_example
+
+
 def run_exp(max_recording_mins, n_recordings,
-            sec_to_cut, duration_recording_mins, max_abs_val, sampling_freq,
-            valid_of_train_fraction, test_fraction,
+            sec_to_cut, duration_recording_mins, max_abs_val,
+            max_min_threshold, max_min_expected, shrink_the_spikes,
+            sampling_freq,
+            low_cut_hz, high_cut_hz,
+            exp_demean, exp_standardize,
+            moving_demean, moving_standardize,
+            channel_demean, channel_standardize,
+            n_folds, i_test_fold,
             model_name,
             input_time_length, final_conv_length,
             batch_size, max_epochs,
@@ -215,13 +392,43 @@ def run_exp(max_recording_mins, n_recordings,
     if max_abs_val is not None:
         preproc_functions.append(lambda data, fs:
                                  (np.clip(data, -max_abs_val, max_abs_val), fs))
+    if max_min_threshold is not None:
+        preproc_functions.append(lambda data, fs:
+                                 (clean_jumps(
+                                     data, 50, max_min_threshold,
+                                     max_min_expected, cuda), fs))
+    if shrink_the_spikes is True:
+        preproc_functions.append(lambda data, fs:
+                                 (shrink_spikes(
+                                     data, 400, 1, 9,), fs))
+
     preproc_functions.append(lambda data, fs: (resampy.resample(data, fs,
                                                                 sampling_freq,
                                                                 axis=1,
                                                                 filter='kaiser_fast'),
                                                sampling_freq))
-    preproc_functions.append(lambda data, fs: (exponential_running_standardize(
-        data.T, factor_new=0.001).T, fs))
+    preproc_functions.append(lambda data, fs:
+                             (bandpass_cnt(data, low_cut_hz, high_cut_hz,
+                                           fs,
+                                           filt_order=4, axis=1),
+                              fs))
+
+    if exp_demean:
+        preproc_functions.append(lambda data, fs: (exponential_running_demean(
+            data.T, factor_new=0.001, init_block_size=100).T, fs))
+    if exp_standardize:
+        preproc_functions.append(lambda data, fs: (exponential_running_standardize(
+            data.T, factor_new=0.001, init_block_size=100).T, fs))
+    if moving_demean:
+        preproc_functions.append(lambda data, fs: (padded_moving_demean(
+            data, axis=1, n_window=201), fs))
+    if moving_standardize:
+        preproc_functions.append(lambda data, fs: (padded_moving_standardize(
+            data, axis=1, n_window=201), fs))
+    if channel_demean:
+        preproc_functions.append(lambda data, fs: (demean(data, axis=1), fs))
+    if channel_standardize:
+        preproc_functions.append(lambda data, fs: (standardize(data, axis=1), fs))
 
     dataset = DiagnosisSet(n_recordings=n_recordings,
                         max_recording_mins=max_recording_mins,
@@ -229,7 +436,7 @@ def run_exp(max_recording_mins, n_recordings,
     if not only_return_exp:
         X,y = dataset.load()
 
-    splitter = Splitter(test_fraction, valid_of_train_fraction)
+    splitter = Splitter(n_folds, i_test_fold,)
     if not only_return_exp:
         train_set, valid_set, test_set = splitter.split(X,y)
         del X,y # shouldn't be necessary, but just to make sure
@@ -288,8 +495,14 @@ def run_exp(max_recording_mins, n_recordings,
 
 
 def run(ex, max_recording_mins, n_recordings,
-        sec_to_cut, duration_recording_mins, max_abs_val, sampling_freq,
-        valid_of_train_fraction, test_fraction,
+        sec_to_cut, duration_recording_mins, max_abs_val,
+        max_min_threshold, max_min_expected, shrink_the_spikes,
+        sampling_freq,
+        low_cut_hz, high_cut_hz,
+        exp_demean, exp_standardize,
+        moving_demean, moving_standardize,
+        channel_demean, channel_standardize,
+        n_folds, i_test_fold,
         model_name, input_time_length, final_conv_length,
         batch_size, max_epochs,
         only_return_exp):
@@ -297,9 +510,15 @@ def run(ex, max_recording_mins, n_recordings,
     ex.info['finished'] = False
     exp = run_exp(
         max_recording_mins, n_recordings,
-        sec_to_cut, duration_recording_mins, max_abs_val, sampling_freq,
-        valid_of_train_fraction=valid_of_train_fraction,
-        test_fraction=test_fraction,
+        sec_to_cut, duration_recording_mins, max_abs_val,
+        max_min_threshold, max_min_expected, shrink_the_spikes,
+        sampling_freq,
+        low_cut_hz, high_cut_hz,
+        exp_demean, exp_standardize,
+        moving_demean, moving_standardize,
+        channel_demean, channel_standardize,
+        n_folds=n_folds,
+        i_test_fold=i_test_fold,
         model_name=model_name,
         input_time_length=input_time_length,
         final_conv_length=final_conv_length,
